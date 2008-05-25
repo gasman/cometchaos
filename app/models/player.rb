1b47f152d4691f5868784830d7714a21db7a7690
matthew <matthew@e9b8adc4-4640-47d0-ab3f-4b6ad088a58c>
2008-05-25 19:46:39 +0000

    choosing avatars

--- app/models/player.rb
+++ app/models/player.rb
@@ -4,17 +4,23 @@ class Player < ActiveRecord::Base
 	
 	has_many :sprites
 	has_one :wizard_sprite, :class_name => 'Sprite', :conditions => "is_wizard = 't'"
+	
+	WIZARD_TYPES = %w(pointer snooker treetrunk molotov elvis nightie ghostie sticky)
+	WIZARD_COLOURS = %w(red magenta green cyan gold yellow grey white)
+	attr_accessor :wizard_type, :wizard_colour
 
 	validates_presence_of :name
-	validates_presence_of :game
+	validates_presence_of :game	
 	# the Rails documentation says I should be validating game_id instead, because
 	# game may not have been set if the association was made some other way besides
 	# the belongs_to association (such as game.players << player). But then it
 	# won't work *at all* if game is a new record.
 	# The Rails documentation can bite me.
+	validates_inclusion_of :wizard_type, :in => WIZARD_TYPES
+	validates_inclusion_of :wizard_colour, :in => WIZARD_COLOURS
 	
 	def after_create
-		self.wizard_sprite = Sprite.new(:image => 'wizards/pointer.png', :is_wizard => true)
+		self.wizard_sprite = Sprite.new(:image => "wizards/#{@wizard_type}_#{@wizard_colour}.png", :is_wizard => true)
 		game.set_wizard_start_positions
 	end
 end
