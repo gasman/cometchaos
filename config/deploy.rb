70330549a988fa2715910a128bf634584305cd93
matthew <matthew@e9b8adc4-4640-47d0-ab3f-4b6ad088a58c>
2008-05-28 23:40:48 +0000

    update deploy.rb for passenger: http://jimneath.org/2008/05/10/using-capistrano-with-passenger-mod_rails/ , http://tomcopeland.blogs.com/juniordeveloper/2008/05/mod_rails-and-c.html

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,30 @@
+# Application
 set :application, "chaos"
-set :repository,  "http://svn.matt.west.co.tt/svn/chaos/trunk/"
-
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
 set :deploy_to, "/var/www/#{application}"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+# Settings
+default_run_options[:pty] = true
+set :use_sudo, false
 
-role :app, "altaria.vm.bytemark.co.uk"
-role :web, "altaria.vm.bytemark.co.uk"
-role :db,  "altaria.vm.bytemark.co.uk", :primary => true
+# Servers
+set :user, "root"
+set :domain, "chaos.zxdemo.org"
+server "altaria.vm.bytemark.co.uk", :app, :web
+role :db, "altaria.vm.bytemark.co.uk", :primary => true
+
+# Subversion
+set :repository,  "http://svn.matt.west.co.tt/svn/chaos/trunk/"
+set :checkout, "export"
+
+# Passenger
+namespace :deploy do
+	desc "Restarting mod_rails with restart.txt"
+	task :restart, :roles => :app, :except => { :no_release => true } do
+		run "touch #{current_path}/tmp/restart.txt"
+	end
+
+	[:start, :stop].each do |t|
+		desc "#{t} task is a no-op with mod_rails"
+		task t, :roles => :app do ; end
+	end
+end
